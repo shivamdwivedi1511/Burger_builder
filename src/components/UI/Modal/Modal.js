05675158bd523d0b067ffbee5b3aa8d800d9b2ad
Shivam Dwivedi <shivamdwivedi1511@gmail.com>
2018-12-13 11:05:06 +0530

    update Modal.js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -11,7 +11,7 @@ const Modal =(props)=>{
         <div className={classes.Modal}
     style={{
         transform:props.show?'translateY(0)':'translateY(-100Vh)',
-        opacity:props.show?'1':'0'
+        opacity:props.show?'1':'0',
     }}>
         {props.children}
     </div>
